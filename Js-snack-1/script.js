40aa835874400d95c4a43b7db725c878d1b10c0c
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-14 14:44:35 +0100

    Condizione che devi inserire un numero

--- Js-snack-1/script.js
+++ Js-snack-1/script.js
@@ -4,11 +4,15 @@ let prompt2 = prompt("Inserire un altro numero");
 let numero1 = parseInt(prompt1)
 let numero2 = parseInt(prompt2)
 
-if (numero1<numero2){
+if(isNaN(numero1 && numero2)){
+    console.log("Non hai inserito un numero")
+}else if(numero1<numero2){
     console.log("Il primo numero è minore del secondo")
-}else{
+}else {
     console.log("Il primo numero è maggiore del secondo")
 }
+
+
 
 
 
