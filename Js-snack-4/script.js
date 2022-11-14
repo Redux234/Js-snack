4bb769cbb90816683be451e02f4c0a1807ab2177
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-14 15:31:50 +0100

    snack 4 con bonus

---
+++ Js-snack-4/script.js
@@ -0,0 +1,10 @@
+let nomeutenteinseito = prompt("Inserisci il tuo nome")
+let nomeutente = nomeutenteinseito.toLocaleLowerCase();
+let invitati = ['anna', 'luca', 'fderico', 'riccardo', 'marco', 'emanuele']
+
+if(nomeutente=invitati){
+    console.log("Evviva sei invitato!!!")
+}else{
+    console.log("Ci dispiace ma non sei invitato")
+}
+
