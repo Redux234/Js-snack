de45ceca27975d82c2a5867391b6a149d63aa92e
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-14 15:42:18 +0100

    errore nome

---
+++ Js-snacks-5/script.js
@@ -0,0 +1,21 @@
+let numeridispariinseriti = []
+let numeroinserito1 = prompt("Inserire un numero")
+let numeroinserito2 = prompt("Inserire un numero")
+let numeroinserito3 = prompt("Inserire un numero")
+let numeroinserito4 = prompt("Inserire un numero")
+let numeroinserito5 = prompt("Inserire un numero")
+let numeroinserito6 = prompt("Inserire un numero")
+
+let numero1 = parseInt(numeroinserito1)
+let numero2 = parseInt(numeroinserito2)
+let numero3 = parseInt(numeroinserito3)
+let numero4 = parseInt(numeroinserito4)
+let numero5 = parseInt(numeroinserito5)
+let numero6= parseInt(numeroinserito6)
+
+if((((numero1||numero2||numero3||numero4||numero5||numero6)%2)=0)){
+
+}else{
+    
+}
+
