b81c778c69dc40bd32683a4faf271e5d946a0671
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-14 14:35:13 +0100

    Base snack 1

---
+++ Js-snack-1/script.js
@@ -0,0 +1,14 @@
+let prompt1 = prompt("Inserire un  numero");
+let prompt2 = prompt("Inserire un altro numero");
+
+let numero1 = parseInt(prompt1)
+let numero2 = parseInt(prompt2)
+
+if (numero1<numero2){
+    console.log("Il primo numero è minore del secondo")
+}else{
+    console.log("Il primo numero è maggiore del secondo")
+}
+
+
+
